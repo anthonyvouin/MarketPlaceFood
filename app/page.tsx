14267c6066d01056cf5e054954ce1b25c8fab533
matthias <matthias.faucon@orange.fr>
2024-10-06 22:00:24 +0200

    fix: design home page

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
 
   const bgColors = ['bg-tertiaryColorPink', 'bg-tertiaryColorOrange', 'bg-tertiaryColorBlue', 'bg-tertiaryColorPurple'];
   return (
-    <div className="flex w-full bg-primaryBackgroundColor h-[85vh]">
-      <section className="grid grid-cols-4 gap-10 px-20 py-10">
+    <div className="flex w-full bg-primaryBackgroundColor min-h-[85vh]">
+      <section className="grid grid-cols-4 gap-10 px-20 py-10 w-full">
 
         {products.map((product, index) => (
           <ProductCard key={product.id} product={product} bgColor={bgColors[index % bgColors.length]} />
