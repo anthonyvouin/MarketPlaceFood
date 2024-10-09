af0c68201d1fb5fac366ba3ab0881913c62021e8
akia <ch.royer15@gmail.com>
2024-10-10 00:45:17 +0200

    remove google sign methode

--- app/components/auth/ButtonsProvider.tsx
+++ app/components/auth/ButtonsProvider.tsx
@@ -4,14 +4,10 @@ import {signIn} from "next-auth/react";
 
 export default function ButtonsProvider() {
 
-    const auth = ()=>{
-        signIn('google').then(res => console.warn(res))
-    }
-
     return (
         <div>
             <button className="google-button flex items-center justify-center"
-                    onClick={() => auth()} type="button">
+                    onClick={() => signIn('google')} type="button">
                 <Image src="/images/googleLogo.png" width={40} height={40} alt="logo google"/>
 
                 Continuer avec google
