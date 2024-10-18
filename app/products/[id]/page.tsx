acc75a3ce65406569b1fa2babc0065ef027945ae
akia <ch.royer15@gmail.com>
2024-10-18 22:17:29 +0200

    format code

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import { getProductById } from "@/app/services/products/product";
-import { useEffect, useState } from "react";
-import { useParams, useRouter } from 'next/navigation'
-import { ProductDto } from "@/app/interface/product/productDto";
+import {getProductById} from "@/app/services/products/product";
+import {useEffect, useState} from "react";
+import {useParams, useRouter} from 'next/navigation'
+import {ProductDto} from "@/app/interface/product/productDto";
 import Image from "next/image";
-import { Button, Heading } from "@radix-ui/themes";
-import { formatPrice } from "@/app/pipe/format";
+import {Button, Heading} from "@radix-ui/themes";
+import {formatPrice} from "@/app/pipe/format";
 
 export default function Product() {
     const [product, setProduct] = useState<ProductDto | null>(null);
-    const { id } = useParams() as { id: string };
+    const {id} = useParams() as { id: string };
     const router = useRouter();
 
-    useEffect(() => {
+    useEffect((): void => {
         const fetchProduct = async () => {
             const product = await getProductById(id);
             return setProduct(product);
@@ -32,17 +32,17 @@ export default function Product() {
                     <h1 className="font-bold text-3xl pt-10">Détails du produit</h1>
                 </div>
                 <div className="mt-20 flex items-center justify-around">
-                    <Image src={product?.image ? product?.image : "/images/default-image.png"} alt={product?.name ?? 'Product Image'} width={100} height={100} />
+                    <Image src={product?.image ? product?.image : "/images/default-image.png"} alt={product?.name ?? 'Product Image'} width={100} height={100}/>
                     <div className="flex flex-col gap-5">
                         <div className="flex flex-col gap-4">
                             <div className="flex flex-col gap-2">
                                 <Heading>{product?.name}</Heading>
                                 <p>{product?.description}</p>
                             </div>
-                            { product?.price && <p className="font-semibold">{formatPrice(product?.price)} €</p> }
-                            
+                            {product?.price && <p className="font-semibold">{formatPrice(product?.price)} €</p>}
+
                         </div>
-                        <hr className="border-2 border-black rounded-full" />
+                        <hr className="border-2 border-black rounded-full"/>
                         <div className="flex flex-col gap-3 rounded-md">
                             <h1 className="font-bold text-xl">Catégorie</h1>
                             <p>{product?.category?.name}</p>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,119 +1,118 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { filterProduct } from '../services/products/product';
+import {useEffect, useState} from 'react';
+import {filterProduct} from '../services/products/product';
 import ProductCard from '../components/ProductCard';
-import { Slider } from '@radix-ui/themes';
+import {Slider} from '@radix-ui/themes';
 import * as Checkbox from '@radix-ui/react-checkbox';
-import { CheckIcon } from '@radix-ui/react-icons';
-import { getAllCategories } from '../services/category/category';
-import { ProductDto } from '../interface/product/productDto';
-import { CategoryDto } from '../interface/category/categoryDto';
+import {CheckIcon} from '@radix-ui/react-icons';
+import {getAllCategories} from '../services/category/category';
+import {ProductDto} from '../interface/product/productDto';
+import {CategoryDto} from '../interface/category/categoryDto';
 
 export default function Products() {
-  const [products, setProducts] = useState<ProductDto[]>([]);
-  const [filters, setFilters] = useState<{ [key in keyof ProductDto]?: any }>({});
-  const [priceRange, setPriceRange] = useState([0, 50]);
-  const [categories, setCategories] = useState<CategoryDto[]>([]);
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+    const [products, setProducts] = useState<ProductDto[]>([]);
+    const [filters, setFilters] = useState<{ [key in keyof ProductDto]?: any }>({});
+    const [priceRange, setPriceRange] = useState([0, 50]);
+    const [categories, setCategories] = useState<CategoryDto[]>([]);
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
-  const bgColors = ['bg-tertiaryColorPink', 'bg-tertiaryColorOrange', 'bg-tertiaryColorBlue', 'bg-tertiaryColorPurple'];
+    const bgColors = ['bg-tertiaryColorPink', 'bg-tertiaryColorOrange', 'bg-tertiaryColorBlue', 'bg-tertiaryColorPurple'];
 
-  const fetchProducts = async (filters: any) => {
-    try {
-      const filteredProducts = await filterProduct(filters);
-      setCategories(await getAllCategories());
-      setProducts(filteredProducts);
-    } catch (error) {
-      console.error("Erreur lors de la récupération des produits :", error);
-    }
-  };
+    const fetchProducts = async (filters: any): Promise<void> => {
+        try {
+            const filteredProducts: ProductDto[] = await filterProduct(filters);
+            setCategories(await getAllCategories());
+            setProducts(filteredProducts);
+        } catch (error) {
+            console.error("Erreur lors de la récupération des produits :", error);
+        }
+    };
 
-  useEffect(() => {
-    if (selectedCategories.length > 0) {
-      handleFilterChange('categoryId', { in: selectedCategories });
-    } else {
-      handleFilterChange('categoryId', undefined); // Si aucune catégorie sélectionnée, pas de filtre sur la catégorie
-    }
-  }, [selectedCategories]);
+    useEffect((): void => {
+        if (selectedCategories.length > 0) {
+            handleFilterChange('categoryId', {in: selectedCategories});
+        } else {
+            handleFilterChange('categoryId', undefined); // Si aucune catégorie sélectionnée, pas de filtre sur la catégorie
+        }
+    }, [selectedCategories]);
 
-  useEffect(() => {
-    fetchProducts(filters);
-  }, [filters]);
+    useEffect(() => {
+        fetchProducts(filters);
+    }, [filters]);
 
-  const handleFilterChange = (key: keyof ProductDto, value: any) => {
-    setFilters(prev => ({ ...prev, [key]: value }));
-  };
+    const handleFilterChange = (key: keyof ProductDto, value: any): void => {
+        setFilters(prev => ({...prev, [key]: value}));
+    };
 
-  const handlePriceChange = (value: number[]) => {
-    setPriceRange(value); // Met à jour la plage de prix localement
-    handleFilterChange('price', { gte: value[0], lte: value[1] });
-  };
+    const handlePriceChange = (value: number[]): void => {
+        setPriceRange(value);
+        handleFilterChange('price', {gte: value[0], lte: value[1]});
+    };
 
-  const handleCategoryChange = (categoryId: string) => {
-    if (selectedCategories.includes(categoryId)) {
-      setSelectedCategories(selectedCategories.filter(id => id !== categoryId));
-    } else {
-      setSelectedCategories([...selectedCategories, categoryId]);
-    }
-  };
+    const handleCategoryChange = (categoryId: string): void => {
+        if (selectedCategories.includes(categoryId)) {
+            setSelectedCategories(selectedCategories.filter(id => id !== categoryId));
+        } else {
+            setSelectedCategories([...selectedCategories, categoryId]);
+        }
+    };
 
-  return (
-    <section className="px-20 py-10 w-full">
-      <div className="mb-8">
-        <h2 className="text-xl font-bold mb-4">Filtres</h2>
-        <div className="mb-6">
-          <h3 className="text-lg font-semibold mb-2">Prix</h3>
-          {priceRange[0]} - {priceRange[1]} €
-          <Slider
-            defaultValue={[0, 50]}
-            min={0}
-            max={50}
-            step={1}
-            value={priceRange}
-            onValueChange={handlePriceChange}
-          />
+    return (
+        <section className="px-20 py-10 w-full">
+            <div className="mb-8">
+                <h2 className="text-xl font-bold mb-4">Filtres</h2>
+                <div className="mb-6">
+                    <h3 className="text-lg font-semibold mb-2">Prix</h3>
+                    {priceRange[0]} - {priceRange[1]} €
+                    <Slider
+                        defaultValue={[0, 50]}
+                        min={0}
+                        max={50}
+                        step={1}
+                        value={priceRange}
+                        onValueChange={handlePriceChange}
+                    />
 
-          <div className="flex justify-between mt-2">
-            <span>0 €</span>
-            <span>50 €</span>
-          </div>
-        </div>
+                    <div className="flex justify-between mt-2">
+                        <span>0 €</span>
+                        <span>50 €</span>
+                    </div>
+                </div>
 
-        <div className="mb-6">
-          <h3 className="text-lg font-semibold mb-2">Catégories</h3>
-          <div>
-            {categories.map(category => (
-              <div key={category.id} className="flex items-center space-x-2 mt-2">
-                <Checkbox.Root
-                  id={category.id}
-                  checked={selectedCategories.includes(category.id)} // Vérifie si la catégorie est sélectionnée
-                  onCheckedChange={() => handleCategoryChange(category.id)}
-                  className="w-5 h-5 bg-gray-200 rounded" >
-                  <Checkbox.Indicator>
-                    <CheckIcon className="text-blue-500 w-4 h-4" />
-                  </Checkbox.Indicator>
-                </Checkbox.Root>
-                <label htmlFor={category.id} className="text-sm">{category.name}</label>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+                <div className="mb-6">
+                    <h3 className="text-lg font-semibold mb-2">Catégories</h3>
+                    <div>
+                        {categories.map(category => (
+                            <div key={category.id} className="flex items-center space-x-2 mt-2">
+                                <Checkbox.Root
+                                    id={category.id}
+                                    checked={selectedCategories.includes(category.id!)} // Vérifie si la catégorie est sélectionnée
+                                    onCheckedChange={() => handleCategoryChange(category.id!)}
+                                    className="w-5 h-5 bg-gray-200 rounded">
+                                    <Checkbox.Indicator>
+                                        <CheckIcon className="text-blue-500 w-4 h-4"/>
+                                    </Checkbox.Indicator>
+                                </Checkbox.Root>
+                                <label htmlFor={category.id} className="text-sm">{category.name}</label>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
 
-      <div className="grid grid-cols-4 gap-10">
-        {products.map((product, index) => (
-          <>
-          <p>{product.id}</p>
-          <ProductCard
-            productId={product.id}
-            key={product.id}
-            product={product}
-            bgColor={bgColors[index % bgColors.length]}
-            />
-          </>
-        ))}
-      </div>
-    </section>
-  );
+            <div className="grid grid-cols-4 gap-10">
+                {products.map((product: ProductDto, index) => (
+                    <>
+                        <ProductCard
+                            productId={product.id!}
+                            key={product.id}
+                            product={product}
+                            bgColor={bgColors[index % bgColors.length]}
+                        />
+                    </>
+                ))}
+            </div>
+        </section>
+    );
 }
