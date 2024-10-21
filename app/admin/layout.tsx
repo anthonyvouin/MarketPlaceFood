4e70aa29d809c1f57b0beece4d3f53780eebfc8e
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-10-21 11:09:28 +0200

    add_chart_js

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -9,48 +9,61 @@ export default function AdminLayout({children}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+      <html lang="en">
         <Head>
-            <link
-                href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;700&display=swap"
-                rel="stylesheet"
-            />
+          <link
+            href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
-        <Theme>
+          <Theme>
             <div className="flex  h-screen">
-                <nav className="flex flex-col width-15  bg-light">
-                    <Link href="/admin/dashboard"
-                          className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation">
-                        Dashboard
-                    </Link>
-                    <Link href="/admin/category"
-                          className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation">
-                        Categories
-                    </Link>
-                    <Link href="/admin/users"
-                          className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation">
-                        Clients
-                    </Link>
-                    <Link href="/admin/contact"
-                          className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation">
-                        Contacts
-                    </Link>
-                    <Link href="/admin/product"
-                          className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation">
-                        Produits
-                    </Link>
-                </nav>
+              <nav className="flex flex-col width-15  bg-light">
+                <Link
+                  href="/admin/dashboard"
+                  className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation"
+                >
+                  Dashboard
+                </Link>
+                <Link
+                  href="/admin/category"
+                  className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation"
+                >
+                  Categories
+                </Link>
+                <Link
+                  href="/admin/users"
+                  className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation"
+                >
+                  Clients
+                </Link>
+                <Link
+                  href="/admin/contact"
+                  className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation"
+                >
+                  Contacts
+                </Link>
+                <Link
+                  href="/admin/product"
+                  className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation"
+                >
+                  Produits
+                </Link>
+                <Link
+                  href="/admin/chart"
+                  className="nav-title text-lg p-2.5 font-semibold text-primaryColor relative underline-animation"
+                >
+                  Graphique
+                </Link>
+              </nav>
 
-                <div className="width-85">
-                    <SessionWrapper>
-                        {children}
-                    </SessionWrapper>
-                </div>
-
+              <div className="width-85">
+                <SessionWrapper>{children}</SessionWrapper>
+              </div>
             </div>
-        </Theme>
+          </Theme>
         </body>
-        </html>
+      </html>
     );
 }
