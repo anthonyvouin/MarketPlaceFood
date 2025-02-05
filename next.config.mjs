77fcd09f02b0c2cd6d9275cf6b6fe46f05961734
anthony vouin <avouin@normandiewebschool.fr>
2025-02-05 11:56:19 +0100

    init pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+      disableDevLogs: true,
+    },
+  });
+  
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -34,4 +47,4 @@ const nextConfig = {
     }
 };
 
-export default nextConfig;
+export default  withPWA(nextConfig);
